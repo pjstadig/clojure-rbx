5a6b6c117f61b589422beb97d6dbf9acb1aa3041
Paul Stadig <paul@stadig.name>
2013-11-10 06:18:06 -0500

    Implement Symbol.

---
+++ lib/clojure/symbol.rb
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright © 2013 Paul Stadig. All rights reserved.
+#
+# The use and distribution terms for this software are covered by the Eclipse
+# Public License 1.0 (http://www.eclipse.org/legal/epl-v10.html) the text of
+# which can be found in the file epl-v10.txt at the root of this distribution.
+# By using this software in any fashion, you are agreeing to be bound by the
+# terms of this license.  You must not remove this notice, or any other, from
+# this software.
+module Clojure
+  class Symbol
+    class << self
+      private :new
+
+      def create(*args)
+        namespace, name = *args
+        namespace, name = nil, namespace if name.nil?
+        if namespace && namespace !~ /\A[^\/\s0-9][^\/\s]*\z/
+          raise ArgumentError, "invalid namespace #{namespace}"
+        end
+        if name !~ /\A[^\s0-9][^\s]*\z/
+          raise ArgumentError, "invalid name #{name}"
+        end
+        new(namespace && namespace.dup, name.dup).freeze
+      end
+    end
+
+    attr_reader :namespace, :name
+
+    def initialize(namespace, name)
+      @namespace = namespace && namespace.freeze
+      @name = name.freeze
+    end
+
+    def ==(obj)
+      obj.is_a?(Symbol) && @namespace == obj.namespace && @name == obj.name
+    end
+
+    def to_s
+      if namespace
+        "#{namespace}/#{name}"
+      else
+        name
+      end
+    end
+  end
+end
